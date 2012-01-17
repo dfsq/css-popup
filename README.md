ec072c32fa0c08c8b4d3e7a9a4543dd59ecf9ea4
Aliaksandr Astashenkau <Alexander.Astashenkov@nordeck.net>
2012-01-17 17:54:54 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# jQuery imageHalftone plugin
+
+Basic lightweight popup plugin for jQuery. All popup positioning within the screen is done with CSS only. No offset or top calculations, only CSS.
+
+## Available options
+
+* closeBtn: [Boolean:true] Show popup close button or not.
+* hideOnOverlayClick: [Boolean:false] Whether to close popup on overlay click.
+* hideOnContentClick: [Boolean:false] Whether to close popup on popup area click.
+* onShow: [Function:null] Callback fired after popup is opened.
+* onClose: [Function:null] Callback fired after popup is closed.
+
+## Example of usage
+
+Use with dynamic DOM elements:
+
+```javascript
+$('<div class="toast">Some dynamic content</div>').cssPopup();
+```
+
+Pass some options:
+
+```javascript
+$('<div class="toast">Settings saved</div>').cssPopup({
+    closeBtn: false,
+    hideOnOverlayClick: true
+});
+```
+
+Or add some animation:
+
+```javasscript
+$('<div class="toast">Settings saved</div>').cssPopup({
+    closeBtn: false,
+    hideOnOverlayClick: true,
+    onShow: function(popup) {
+        popup.delay(2000).fadeOut(function() {
+            $.cssPopup.close();
+        });
+    }
+});
+```
+
+Display arbitrary HTML content:
+
+```javascript
+<script type="text/template" id="popup-content">
+    <!-- Some HTML here -->
+    <a class="csspopup-close">Custom close button</a>
+</script>
+var content = $('#popup-content').html();
+$.cssPopup(content, {
+    closeBtn: false
+});
+```
---
+++ js/jquery.csspopup.js
@@ -0,0 +1,98 @@
+/**
+ * jQuery CSS Popup plugin.
+ */
+;(function($) {
+
+	var overlay, popup, cont, options;
+
+	var create = function(content, opts) {
+
+		var _options = opts ? $.extend({}, options, opts) : options;
+
+		// Create and append elements
+		overlay = $('<div class="csspopup-overlay"></div>');
+		popup   = $('<div class="csspopup-popup"><div class="csspopup-content"></div></div>');
+		cont    = popup.find('.csspopup-content');
+		_options.closeBtn && popup.prepend('<div class="csspopup-close">&times;</div>');
+		cont.append(content);
+
+		overlay.append(popup).appendTo('body');
+
+		// IE fix
+		if ($.browser.msie && parseInt($.browser.version) < 9) {
+			overlay.before('<div class="csspopup-overlay csspopup-overlay-ie"></div>')
+				   .append('<div class="csspopup-valignfix"></div>');
+			overlay = $('.csspopup-overlay');
+		}
+
+		// Bind overlay events
+		popup.click(function(e) {
+			e.stopPropagation();
+			if (_options.hideOnContentClick) {
+				$.cssPopup.close(_options.onClose);
+			}
+		});
+
+		if (_options.hideOnOverlayClick) {
+			overlay.click(function() {
+				$.cssPopup.close(_options.onClose);
+			});
+		}
+
+		popup.find('.csspopup-close').click(function() {
+			$.cssPopup.close(_options.onClose);
+		});
+
+		// Show overlay
+		$.cssPopup.show(_options.onShow);
+	};
+
+	/**
+	 * Plugin code.
+	 * @param {Object} opts
+	 */
+	$.fn.cssPopup = function(opts) {
+		var content = $(this).clone();
+		create(content, opts);
+		return this;
+	};
+
+	/**
+	 * Global function.
+	 * @param {String} content Html content of the popup.
+	 */
+	$.cssPopup = function(content, opts) {
+		create(content, opts);
+	};
+
+	/**
+	 * Global pubic show popup function.
+	 * @param {Function} callback Callback function to be called.
+	 */
+	$.cssPopup.show = function(callback) {
+		overlay.show();
+		var cb = callback ? callback : options.onShow;
+		cb && cb(popup);
+	};
+
+	/**
+	 * Global public close popup method.
+	 */
+	$.cssPopup.close = function(callback) {
+		var cb = callback ? callback : options.onClose;
+		cb && cb(popup);
+		overlay.remove();
+	};
+
+	/**
+	 * Default settings.
+	 */
+	$.cssPopup.defaults = options = {
+		closeBtn: true,
+		hideOnOverlayClick: false,
+		hideOnContentClick: false,
+		onShow: null,
+		onClose: null
+	};
+
+})(jQuery);
